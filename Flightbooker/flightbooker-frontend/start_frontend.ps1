cb9e3b98b0a160edae0e8f3b0b14767db62c709c
lxm11r <leonie.moeller@maibornwolff.de>
2022-09-30 11:55:56 +0200

    port changes

--- Flightbooker/flightbooker-frontend/start_frontend.ps1
+++ Flightbooker/flightbooker-frontend/start_frontend.ps1
@@ -1,1 +1,1 @@
-dapr run --app-port 5173 --app-id flightbooker-frontend --app-protocol http --dapr-http-port 3502 -- npm run dev
+dapr run --app-port 5173 --app-id flightbooker-frontend --app-protocol http --dapr-http-port 3501 -- npm run dev
