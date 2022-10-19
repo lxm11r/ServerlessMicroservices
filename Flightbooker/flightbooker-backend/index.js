0bdf8ccf6c3916d1f7e45afa5cfcb7a100753cc9
lxm11r <leonie.moeller@maibornwolff.de>
2022-10-19 14:40:57 +0200

    add environment variables

--- Flightbooker/flightbooker-backend/index.js
+++ Flightbooker/flightbooker-backend/index.js
@@ -15,6 +15,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 port = 3000;
+//port = 443;
 
 app.use(express.json());
 
--- Flightbooker/flightbooker-frontend/src/services/BookingService.js
+++ Flightbooker/flightbooker-frontend/src/services/BookingService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+//import { loadEnv } from "vite";
 
-const DAPR_HOST = import.meta.env.DAPR_HOST || "http://localhost";
-const DAPR_HTTP_PORT = import.meta.env.DAPR_HTTP_PORT || "3000";
+const DAPR_HOST = import.meta.env.VITE_DAPR_HOST || "http://localhost";
+const DAPR_HTTP_PORT = import.meta.env.VITE_DAPR_HTTP_PORT || "3000"; //was: 3000
 
 let axiosConfig = {
     headers: {
@@ -10,6 +11,7 @@ let axiosConfig = {
 };
 
 export async function getAllSeats() {
+    console.log(DAPR_HOST);
     const response = await axios.get(`${DAPR_HOST}:${DAPR_HTTP_PORT}/api/seats`, axiosConfig);
     return response.data;
 }
